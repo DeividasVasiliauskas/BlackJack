539fd581faf25b5612eeeb9fc549e1d4d5afb9d7
DeividasVasiliauskas <94546563+DeividasVasiliauskas@users.noreply.github.com>
2021-11-18 10:44:34 +0200

    fixed major bugs, added TODO

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def real_cards(cards):
     face = ["T", "J", "Q", "K"]
     r_cards = []
     for i in cards:
-        if i == 11:
+        if i == 11 or i == 1:
             r_cards.append("A" + suit[random.randint(0, 3)])
         elif i == 10:
             r_cards.append(face[random.randint(0, 3)] + suit[random.randint(0, 3)])
@@ -36,20 +36,18 @@ def sum_cards(list_cards):
 
 def hit(dict_key):
     """When hit is called providing a key - player/dealer - adds a card acordingly"""
+    # TODO return a new updated dict
     game_dict[dict_key].append(cards[random.randint(0, len(cards) - 1)])
     print("Card was added")
 
 
 def dealer(i):
-    print(f"Dealer reveals: {real_cards(d_cards)[0]} {real_cards(d_cards)[1]}")
     card_sum = sum_cards(i)
     while sum_cards(i) < 16:
         print("Sum bellow 16 HITTING")
         hit("dealer")
-        print(game_dict["dealer"])
+        print(real_cards(i))
         card_sum = sum_cards(i)
-    if sum_cards(i) > 21:
-        card_sum = 0
     return card_sum
 
 
@@ -59,7 +57,7 @@ game_dict = {"dealer": [],
 
 
 def play_again():
-    finished = input("Do you wish to play agai? Y/N: ").lower()
+    finished = input("Do you wish to play again? Y/N: ").lower()
 
     if finished == 'y':
         game_dict["player"] = []
@@ -79,7 +77,7 @@ def game():
     print(f"Your cards: {real_p_cards[0]} {real_p_cards[1]}\nDealer cards: {real_d_cards[0]} X")
 
     if sum_cards(p_cards) > 21:
-        finished = input("You have more then 21,You lose! Do you wish to play agai? Y/N: ").lower()
+        finished = input("You have more then 21,You lose! Do you wish to play again? Y/N: ").lower()
 
         if finished == 'y':
             game_dict["player"] = []
@@ -98,16 +96,18 @@ def game():
                 hitting = False
 
         else:
+            print(f"Your cards {real_p_cards}")
             hit_stand = input(f"You have {sum_cards(p_cards)}, HIT or STAND?: ").lower()
         if hit_stand == "hit":
             hit("player")
         else:
             hitting = False
-
+    print(f"Dealer reveals {real_d_cards}")
     dealer_score = dealer(d_cards)
-    if dealer_score > sum_cards(p_cards):
+
+    if sum_cards(p_cards) < dealer_score < 22:
         print(f"You have {sum_cards(p_cards)}, Dealer has {dealer(d_cards)}. You lose!")
-    elif dealer_score < sum_cards(p_cards):
+    elif dealer_score < sum_cards(p_cards) or dealer_score > 21:
         print(f"You have {sum_cards(p_cards)}, Dealer has {dealer(d_cards)}. You Win!")
     else:
         print(f"You have {sum_cards(p_cards)}, Dealer has {dealer(d_cards)}. It\'s a draw!")
@@ -115,4 +115,3 @@ def game():
 
 
 game()
-
